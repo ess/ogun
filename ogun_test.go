02fc926f7119dd8d283eac010fae68015619b7b5
Dennis Walters <dwalters@engineyard.com>
2018-09-24 14:54:26 -0400

    Godog boilerplate

---
+++ ogun_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/ess/ogun/features/steps"
+
+	"github.com/DATA-DOG/godog"
+	"github.com/ess/jamaica"
+	"github.com/ess/kennel"
+	"github.com/ess/mockable"
+	"github.com/ess/ogun/cmd/ogun/cmd"
+	"github.com/ess/testscope"
+)
+
+var commandOutput string
+var lastCommandRanErr error
+
+func TestMain(m *testing.M) {
+	if testscope.Integration() {
+		status := godog.RunWithOptions(
+			"godog",
+			func(s *godog.Suite) {
+				mockable.Enable()
+				jamaica.SetRootCmd(cmd.RootCmd)
+				jamaica.StepUp(s)
+				steps.Register()
+				kennel.StepUp(s)
+			},
+
+			godog.Options{
+				Format:    "pretty",
+				Paths:     []string{"features"},
+				Randomize: time.Now().UTC().UnixNano(),
+			},
+		)
+
+		if st := m.Run(); st > status {
+			status = st
+		}
+
+		os.Exit(status)
+	}
+}
+
+func TestTrue(t *testing.T) {
+}
